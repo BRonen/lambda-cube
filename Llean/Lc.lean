82e85588145306d62cfda42c0ff7f471795dd9e3
BRonen <brennopereira6@gmail.com>
2025-02-17 08:38:13 -0300

    refactor(LC): using unicode

--- Llean/Lc.lean
+++ Llean/Lc.lean
@@ -1,8 +1,8 @@
 inductive Expr where
-  | abs : Expr -> Expr
-  | app : Expr -> Expr -> Expr
-  | var : Nat -> Expr
-  | val : Nat -> Expr
+  | abs : Expr → Expr
+  | app : Expr → Expr → Expr
+  | var : Nat → Expr
+  | val : Nat → Expr
 
 open Expr
 
@@ -19,23 +19,22 @@ def exprToString (e : Expr) : String :=
       s!"({f} {arg})"
 
 instance : ToString Expr where
-  toString : Expr -> String := exprToString
+  toString : Expr → String := exprToString
 
 def test := app (abs (var 0)) (abs (var 0))
-#eval println! test
-
+#eval test
 #eval ((List.cons 1 List.nil).cons 2).drop 1
 
 inductive Value where
-  | value : Nat -> Value
-  | closure : List Value -> Expr -> Value
+  | value : Nat → Value
+  | closure : List Value → Expr → Value
 
 open Value
 
 mutual
   def listSize (l : List Value) : Nat :=
     match l with
-     | []      => 0
+     | [] => 0
      | v :: vs => size v + listSize vs
 
   def size (v : Value) : Nat :=
@@ -61,18 +60,17 @@ def eval' (fuel : Nat) (ctx : List Value) (expr : Expr) : Except String Value :=
   else
     let fuel' := fuel - 1
     match expr with
-     | val v => Except.ok (value v)
+     | val v => Except.ok $ value v
      | var i =>
        match ctx.get? i with
         | some v => Except.ok v
         | none => Except.error s!"Variable not found: {i} inside {expr}"
-     | abs body => Except.ok (closure ctx body)
+     | abs body => Except.ok $ closure ctx body
      | app f arg => do
-       let arg' <- eval' fuel' ctx arg
-       let f' <- eval' fuel' (ctx.cons arg') f
-       match f' with
-        | closure ctx body => eval' fuel' ctx body
-        | other => pure other
+       let arg' ← eval' fuel' ctx arg
+       match ← eval' fuel' ctx f with
+        | closure ctx' body => eval' fuel' (ctx'.cons arg') body
+        | _ => Except.error s!"Trying to apply a non-closure value: {expr}"
 
 def fuelNeeded : Expr → Nat
   | Expr.val _ => 1
@@ -80,10 +78,10 @@ def fuelNeeded : Expr → Nat
   | Expr.abs body => 1 + fuelNeeded body
   | Expr.app f arg => 1 + fuelNeeded f + fuelNeeded arg
 
+#eval fuelNeeded (app (abs (var 0)) (abs (app (var 0) (var 0))))
+#eval fuelNeeded (app (abs (app (abs (var 0)) (val 5))) (val 3))
+
 def eval ctx expr := eval' (fuelNeeded expr) ctx expr
 
-def expr := (app (abs (app (abs (var 0)) (val 5))) (val 3))
-
-#eval expr
-#eval fuelNeeded expr
-#eval eval List.nil expr
+#eval eval List.nil (app (abs (var 0)) (abs (app (var 0) (var 0))))
+#eval eval List.nil (app (abs (app (abs (var 0)) (val 5))) (val 3))
