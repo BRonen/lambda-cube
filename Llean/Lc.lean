8008aef57e020ff40b828fdb8778808ffe39adb0
BRonen <brennopereira6@gmail.com>
2025-02-17 08:38:13 -0300

    feat(LC): termination proof using fuel and sizes

--- Llean/Lc.lean
+++ Llean/Lc.lean
@@ -32,36 +32,58 @@ inductive Value where
 
 open Value
 
-def valueToString (v : Value) : String :=
-  match v with
-   | value v => s!"{v}"
-   | closure ctx body =>
-      let ctx := ctx.map valueToString
+mutual
+  def listSize (l : List Value) : Nat :=
+    match l with
+     | []      => 0
+     | v :: vs => size v + listSize vs
+
+  def size (v : Value) : Nat :=
+    match v with
+     | value _ => 1
+     | closure ctx _ => 1 + listSize ctx
+end
+
+def valueToString (fuel : Nat) (v : Value) : String :=
+  match fuel, v with
+   | 0, _ => ""
+   | _, value v => s!"{v}"
+   | fuel' + 1, closure ctx body =>
+      let ctx := ctx.map (valueToString fuel')
       s!"(τ {ctx} : {body})"
-  decreasing_by sorry
 
 instance : ToString Value where
-  toString : Value -> String := valueToString
+  toString (v : Value) : String := valueToString (size v) v
 
-def eval (ctx : List Value) (expr : Expr) : Option Value :=
-  match expr with
-   | val v => some (value v)
-   | var i =>
-     match ctx.get? i with
-      | some v => some v
-      | none => none
-   | abs body => some (closure ctx body)
-   | app f arg =>
-     match eval ctx arg with
-      | none => none
-      | some param =>
-        let f := eval (ctx.cons param) f
-        match f with
-         | some (closure ctx body) => eval ctx body
-         | f => f
-  decreasing_by sorry
+def eval' (fuel : Nat) (ctx : List Value) (expr : Expr) : Except String Value :=
+  match fuel with
+   | 0 => Except.error "Fuel not enough"
+   | fuel' + 1 =>
+     match expr with
+      | val v => Except.ok (value v)
+      | var i =>
+        match ctx.get? i with
+         | some v => Except.ok v
+         | none => Except.error s!"Variable not found: {i} inside {expr}"
+      | abs body => Except.ok (closure ctx body)
+      | app f arg =>
+        match eval' fuel' ctx arg with
+         | Except.ok arg' =>
+           match eval' fuel' (ctx.cons arg') f with
+            | Except.ok (closure ctx body) => eval' fuel' ctx body
+            | other => other
+         | other => other
 
-#eval eval
-  List.nil
-  (app (abs (app (abs (var 0)) (val 5))) (val 3))
+def fuelNeeded : Expr → Nat
+  | Expr.val _ => 1
+  | Expr.var _ => 1
+  | Expr.abs body => 1 + fuelNeeded body
+  | Expr.app f arg => 1 + fuelNeeded f + fuelNeeded arg
 
+def eval ctx expr := eval' (fuelNeeded expr) ctx expr
+
+def expr := (app (abs (app (abs (var 0)) (val 5))) (val 3))
+
+#eval expr
+#eval fuelNeeded expr
+#eval eval List.nil expr
